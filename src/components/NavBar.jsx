da25222c7a62f7c95734ab9af8ceba7bfcdf3453
Webymind <97634238+webymind@users.noreply.github.com>
2025-02-12 17:47:03 +0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">Movie App</Link>
+        <Link to="/">MauritiusMovie App</Link>
       </div>
       <div className="navbar-links">
         <Link to="/" className="nav-link">
